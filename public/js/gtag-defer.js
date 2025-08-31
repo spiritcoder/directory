63729868c65f0454a48990d75a0a56dfbba1da7e
spiritcoder <ikechukwuapeh@hotmail.com>
2025-08-31 13:20:53 +0100

    gtag

--- public/js/gtag-defer.js
+++ public/js/gtag-defer.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function () {
   setTimeout(function () {
     // Dynamically create the script element
     var script = document.createElement("script");
-    script.src = "https://www.googletagmanager.com/gtag/js?id=G-MDYRVWD00G";
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-C9Q6K2GRCK";
     script.async = true;
     script.defer = true;
 
@@ -17,7 +17,7 @@ window.addEventListener("load", function () {
       window.gtag = gtag; // Make gtag accessible globally
 
       gtag("js", new Date());
-      gtag("config", "G-MDYRVWD00G");
+      gtag("config", "G-C9Q6K2GRCK");
     };
   }, 3000);
 });
